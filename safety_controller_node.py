a96c5e23f86d06d2837679a942077000512f8e14
rdas1 <rdas1@exeter.edu>
2017-07-12 18:43:24 -0400

    hell yeah i built this shit

--- safety_controller_node.py
+++ safety_controller_node.py
@@ -7,30 +7,30 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 
 class SafetyControllerNode:
-    def __init__(self): 
+    def __init__(self):
 	# subscribe to incoming Ackermann drive commands
         rospy.Subscriber("/ackermann_cmd_mux/output", AckermannDriveStamped,self.ackermann_cmd_input_callback)
 	# subscribe to incoming laser scan data
         rospy.Subscriber("/scan", LaserScan, self.laser_callback)
-    	
+
 
         # publisher for the safe Ackermann drive command
-        self.cmd_pub = rospy.Publisher("ackermann_cmd", AckermannDriveStamped, queue_size=10) 
+        self.cmd_pub = rospy.Publisher("ackermann_cmd", AckermannDriveStamped, queue_size=10)
     	self.DRIVE = True
-        self.DANGER_DISTANCE = .5
+        self.DANGER_DISTANCE = 1
         self.ANGLE = 40*np.pi/180
-	
+
     def laser_callback(self,msg):
         angle_min = msg.angle_min
         angle_inc = msg.angle_increment
         ranges = msg.ranges
-	
+
         for index in range(0,len(ranges)):
             angle = angle_min + angle_inc*index
 
             LOW = -self.ANGLE/2
             HIGH = self.ANGLE/2
-	
+
             if angle >LOW and angle < HIGH:
                 if ranges[index] < self.DANGER_DISTANCE:
                     self.DRIVE = False
@@ -50,7 +50,7 @@ class SafetyControllerNode:
             stop_msg.drive.speed = 0
             stop_msg.header.stamp = rospy.Time.now()
             self.cmd_pub.publish(stop_msg)
-            
+
 
 if __name__ == "__main__":
     rospy.init_node("safety_controller")
